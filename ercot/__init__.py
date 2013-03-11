7a216014f3b0d503bc2527c918eafc53f71b86f9
Austin Marshall <oxtopus@gmail.com>
2013-03-10 18:28:25 -0700

    More forgiving treatment of logging config file.  You can use the modules directly (without perforimg an install), and if you do an install, the logging config file is also installed as part of the egg.

--- ercot/__init__.py
+++ ercot/__init__.py
@@ -3,6 +3,11 @@ import logging.config
 
 from pkg_resources import Requirement, resource_filename
 
-logging.config.fileConfig(
-    resource_filename(Requirement.parse('ercot'), 'conf/logging.conf'))
-log = logging.getLogger(__name__)
+try:
+  loggingConf = \
+    resource_filename(Requirement.parse('ercot'), 'conf/logging.conf')
+except Exception as e:
+  loggingConf = resource_filename(__name__, '../conf/logging.conf')
+
+logging.config.fileConfig(loggingConf)
+log = logging.getLogger('ercot')
